eaaa8634b3202d2d8f752dd9fb84c08c09eb88ec
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2024-07-26 18:52:56 -0500

    ok

--- src/main/java/infrastructure/config/DatabaseConfig.java
+++ src/main/java/infrastructure/config/DatabaseConfig.java
@@ -9,7 +9,7 @@ public class DatabaseConfig {
     
     private static final String URL = "jdbc:mysql://localhost:3306/sgpzf";
     private static final String USER = "root";
-    private static final String PASSWORD = "12345";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
